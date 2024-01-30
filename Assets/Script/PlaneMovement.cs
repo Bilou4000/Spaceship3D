02222fdd35cc15e6a61a9779f1ce1cc11e1c34df
Bilou4000 <claire.benes@artfx.fr>
2024-01-30 11:11:15 +0100

    [feat] up and down when rotating

--- Assets/Script/PlaneMovement.cs
+++ Assets/Script/PlaneMovement.cs
@@ -5,38 +5,64 @@ using UnityEngine;
 public class PlaneMovement : MonoBehaviour
 {
     [SerializeField] private float speed, tilt, rotatingYSpeed, rotatingXSpeed;
-    private Quaternion zQuater, yQuater;
+    private Quaternion zQuater, yQuater, xQuater;
     
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += transform.forward * speed * Time.deltaTime;
-            
+            xQuater = new Quaternion((transform.rotation.x - rotatingXSpeed) * Time.deltaTime, 0, 0, 1);     
 
-            
+            //transform.rotation = xQuater * transform.rotation;
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                yQuater = new Quaternion(0, (transform.rotation.y + rotatingYSpeed) * Time.deltaTime, 0, 1);
+                zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, -1);
+
+                transform.rotation = yQuater * xQuater * transform.rotation * zQuater;
+                //transform.rotation = yQuater * transform.rotation * zQuater;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                yQuater = new Quaternion(0, (transform.rotation.y + rotatingYSpeed) * Time.deltaTime, 0, -1);
+                zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, 1);
+
+                transform.rotation = yQuater * xQuater * transform.rotation * zQuater;
+                //transform.rotation = yQuater * transform.rotation * zQuater;
+            }
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.S))
         {
-            yQuater = new Quaternion(0, (transform.rotation.y + rotatingYSpeed) * Time.deltaTime, 0, 1);
-            zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, -1);
+            transform.position += transform.forward * speed * Time.deltaTime;
 
-            transform.rotation = yQuater * transform.rotation * zQuater;
+            xQuater = new Quaternion((-transform.rotation.x + rotatingXSpeed) * Time.deltaTime, 0, 0, 1);
+
+            //transform.rotation = xQuater * transform.rotation;
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                yQuater = new Quaternion(0, (transform.rotation.y + rotatingYSpeed) * Time.deltaTime, 0, 1);
+                zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, -1);
+
+                transform.rotation = yQuater * xQuater * transform.rotation * zQuater;
+                //transform.rotation = yQuater * transform.rotation * zQuater;
+            }
+
+            else if (Input.GetKey(KeyCode.A))
+            {
+                yQuater = new Quaternion(0, (transform.rotation.y + rotatingYSpeed) * Time.deltaTime, 0, -1);
+                zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, 1);
+
+                transform.rotation = yQuater * xQuater * transform.rotation * zQuater;
+                //transform.rotation = yQuater * transform.rotation * zQuater;
+            }
         }
-
-        if (Input.GetKey(KeyCode.A))
+        else
         {
-            yQuater = new Quaternion(0, (transform.rotation.y + rotatingYSpeed) * Time.deltaTime, 0, -1);
-            zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, 1);
-
-            transform.rotation = yQuater * transform.rotation * zQuater;
+            transform.Rotate(-transform.rotation.x, -transform.rotation.y, -transform.rotation.z);
         }
-
-
-        //else
-        //{
-        //    transform.Rotate(-transform.rotation.x, -transform.rotation.y, -transform.rotation.z);
-        //}
     }
 
 }
