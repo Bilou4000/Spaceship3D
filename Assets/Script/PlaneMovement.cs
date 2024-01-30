32058a37dfab23319c9ba5138d5d68996491b027
Bilou4000 <claire.benes@artfx.fr>
2024-01-30 10:33:04 +0100

    [add] beautiful plane

--- Assets/Script/PlaneMovement.cs
+++ Assets/Script/PlaneMovement.cs
@@ -4,43 +4,39 @@ using UnityEngine;
 
 public class PlaneMovement : MonoBehaviour
 {
-    [SerializeField] private float speed, tilt, rotatingSpeed;
-    private Quaternion startRotation;
-    private Quaternion zQuater;
-    private Quaternion yQuater;
-
-    private void Start()
-    {
-        startRotation = transform.rotation;
-    }
-
-
+    [SerializeField] private float speed, tilt, rotatingYSpeed, rotatingXSpeed;
+    private Quaternion zQuater, yQuater;
+    
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += transform.forward * speed * Time.deltaTime;
+            
 
-            if (Input.GetKey(KeyCode.D))
-            {
-                yQuater = new Quaternion(0, (transform.rotation.y + rotatingSpeed) * Time.deltaTime, 0, 1);
-                zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, -1);
-
-                transform.rotation = yQuater * transform.rotation * zQuater;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                yQuater = new Quaternion(0, (transform.rotation.y + rotatingSpeed) * Time.deltaTime, 0, -1);
-                zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, 1);
-
-                transform.rotation = yQuater * transform.rotation * zQuater;
-            }
+            
         }
-        else
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-transform.rotation.x, -transform.rotation.y, -transform.rotation.z);
+            yQuater = new Quaternion(0, (transform.rotation.y + rotatingYSpeed) * Time.deltaTime, 0, 1);
+            zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, -1);
+
+            transform.rotation = yQuater * transform.rotation * zQuater;
         }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            yQuater = new Quaternion(0, (transform.rotation.y + rotatingYSpeed) * Time.deltaTime, 0, -1);
+            zQuater = new Quaternion(0, 0, (transform.rotation.z + tilt) * Time.deltaTime, 1);
+
+            transform.rotation = yQuater * transform.rotation * zQuater;
+        }
+
+
+        //else
+        //{
+        //    transform.Rotate(-transform.rotation.x, -transform.rotation.y, -transform.rotation.z);
+        //}
     }
 
 }
